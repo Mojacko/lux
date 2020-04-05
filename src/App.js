bb3c73006b45c0577b2eb1c9f11457133e176b50
rans <rans>
2020-04-06 05:41:07 +0800

    integrated with firebase, created sign-in/google and sign-out

--- src/App.js
+++ src/App.js
@@ -3,24 +3,46 @@ import "./App.css";
 
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homapge.component";
-import SignInRegister from "./pages/signin-register/signin-register.component";
 import Shop from "./pages/shop/shop.component";
+import SignInRegister from "./pages/signin-register/signin-register.component";
 
+import { auth } from "./firebase/firebase.util";
 
 import { Switch, Route } from "react-router-dom";
 
-function App() {
-    return (
-        <div>
-            <Header/>
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route exact path="/shop" component={Shop} />
-                <Route exact path="/signin" component={SignInRegister} />
-                
-            </Switch>
-        </div>
-    );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            currentUser: null,
+        };
+    }
+
+    unSubscribedFromAuth = null;
+
+    componentDidMount() {
+        this.unSubscribedFromAuth = auth.onAuthStateChanged((user) => {
+            this.setState({ currentUser: user });
+            console.log(user);
+        });
+    }
+
+    componentWillUnmount() {
+        this.unSubscribedFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header currentUser={this.state.currentUser} />
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/shop" component={Shop} />
+                    <Route exact path="/signin" component={SignInRegister} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
